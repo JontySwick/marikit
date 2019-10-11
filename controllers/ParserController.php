ffed26f47ca46d0534c35a16d39c7a268ea00ab5
Jonty Swick <jontyswick@gmail.com>
2019-10-12 00:36:20 +0300

    start parser

--- controllers/ParserController.php
+++ controllers/ParserController.php
@@ -2,13 +2,11 @@
 
 namespace app\controllers;
 
-use Yii;
-use yii\filters\AccessControl;
+use Exception;
+use yii\helpers\Json;
+use yii\helpers\VarDumper;
 use yii\web\Controller;
-use yii\web\Response;
-use yii\filters\VerbFilter;
-use app\models\LoginForm;
-use app\models\ContactForm;
+use linslin\yii2\curl;
 
 class ParserController extends Controller
 {
@@ -26,22 +24,45 @@ class ParserController extends Controller
      * Parse action.
      *
      * @return Response|string
+     * @throws Exception
      */
     public function actionParse()
     {
-        
-        /*if (!Yii::$app->user->isGuest) {
-            return $this->goHome();
+        $lastDate = time();
+        $previewText = '';
+
+        $curl = new curl\Curl();
+        $response = $curl->get('https://www.rbc.ru/v10/ajax/get-news-feed/project/rbcnews/lastDate/' . $lastDate . '/limit/22');
+        $response = Json::decode($response, true);
+
+        foreach ($response['items'] as $arNews){
+            $previewText .= htmlspecialchars($arNews['html']);
         }
 
-        $model = new LoginForm();
-        if ($model->load(Yii::$app->request->post()) && $model->login()) {
-            return $this->goBack();
+        preg_match_all('/href=&quot;(.+)&quot;/', $previewText, $urlMatches, PREG_PATTERN_ORDER);
+        foreach ($urlMatches[1] as $url) {
+            $arUrl = parse_url($url);
+            $arPath = explode('/', $arUrl['path']);
+            $hash = end($arPath);
+            if($hash){
+                $response = $curl->get('https://www.rbc.ru/v10/ajax/news/slide/' . $hash);
+                $response = Json::decode($response, true);
+
+                $news = htmlspecialchars($response['html']);
+
+                VarDumper::dump($url, 10, true);
+                VarDumper::dump($response['html'], 10, true);
+                preg_match_all('/(itemprop=&quot;headline&quot;&gt;(.+)&lt;\/span&gt;)|(&lt;p&gt;(.*)&lt;\/p&gt;)/', $news, $matches, PREG_PATTERN_ORDER);
+                VarDumper::dump($matches, 10, true);
+
+                break;
+            }
         }
 
-        $model->password = '';
-        return $this->render('login', [
-            'model' => $model,
-        ]);*/
+        #https://www.rbc.ru/v10/ajax/news/slide/5da0de1f9a7947d805a90f4b?slide=8
+        #VarDumper::dump($response, 10, true);
+        #var_dump($response);
+        die();
+        return '';
     }
 }
